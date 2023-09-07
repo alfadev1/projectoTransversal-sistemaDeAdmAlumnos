631cfb26b3fa14a97dfc6b14b81ce11ceed9414b
SimonettaDaniel <Simonetta.Daniel@hotmail.com>
2023-09-07 11:59:42 -0300

    ConnectionPatch

--- App/appGestionUniversidad/src/AccesoADatos/Conexion.java
+++ App/appGestionUniversidad/src/AccesoADatos/Conexion.java
@@ -1,13 +1,52 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package AccesoADatos;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
 
 /**
  *
  * @author cyka
  */
 public class Conexion {
+    private static String url="jdbc:mariadb://localhost/proyecto_transversal";
+    private static String usuario="root";
+    private static String password="";
+
+   
+    private static Conexion conexion=null;
+    
+     private Conexion() {
+        try {
+            Class.forName("org.mariadb.jdbc.Driver");
+            
+            
+        } catch (ClassNotFoundException ex) {
+            JOptionPane.showMessageDialog(null, "Clase Conexion: Error al cargar Driver");
+        }
+    }
+
+
+    
+    public static Connection getConexion() {
+        Connection con=null;
+      if(conexion == null){
+          
+           conexion= new Conexion();
+        }
+        try {
+            // Setup the connection with the DB
+            con = DriverManager.getConnection(url + "?useLegacyDatetimeCode=false&serverTimezone=UTC" + "&user=" + usuario + "&password=" + password);
+            JOptionPane.showMessageDialog(null, "conectado");
+        }catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error de conexion ");
+        }
+        
+        return con;
+    }
     
 }
+
