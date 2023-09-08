addcf07296364612085ddac6714517c85b8882f3
SIVAnode <Simonetta.Daniel@hotmail.com>
2023-09-07 21:20:45 -0300

    00

--- App/appGestionUniversidad/src/Entidades/Alumno.java
+++ App/appGestionUniversidad/src/Entidades/Alumno.java
@@ -1,91 +1,11 @@
 package Entidades;
 
-import java.time.LocalDate;
-
 /**
  *
  * @author cyka
  */
 public class Alumno {
-//Ya voy agregando las entidades (Daniel)	
+//No me dejó pushear Entidades al main (Daniel)	
     
-    private int idAlumno;
-    private String nombre;
-    private String apellido;
-    private int dni;
-    private LocalDate fNac;
-    private boolean estado;
-
-    public Alumno(int idAlumno, String nombre, String apellido, int dni, LocalDate fNac, boolean estado) {
-        this.idAlumno = idAlumno;
-        this.nombre = nombre;
-        this.apellido = apellido;
-        this.dni = dni;
-        this.fNac = fNac;
-        this.estado = estado;
-    }
-
-    public Alumno(String nombre, String apellido, int dni, LocalDate fNac, boolean estado) {
-        this.nombre = nombre;
-        this.apellido = apellido;
-        this.dni = dni;
-        this.fNac = fNac;
-        this.estado = estado;
-    }
-
-    public Alumno() {
-    }
-
-    public int getIdAlumno() {
-        return idAlumno;
-    }
-
-    public void setIdAlumno(int idAlumno) {
-        this.idAlumno = idAlumno;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getApellido() {
-        return apellido;
-    }
-
-    public void setApellido(String apellido) {
-        this.apellido = apellido;
-    }
-
-    public int getDni() {
-        return dni;
-    }
-
-    public void setDni(int dni) {
-        this.dni = dni;
-    }
-
-    public LocalDate getfNac() {
-        return fNac;
-    }
-
-    public void setfNac(LocalDate fNac) {
-        this.fNac = fNac;
-    }
-
-    public boolean isEstado() {
-        return estado;
-    }
-
-    public void setEstado(boolean estado) {
-        this.estado = estado;
-    }
-
-    @Override
-    public String toString() {
-        return "Alumno{" + "idAlumno=" + idAlumno + ", nombre=" + nombre + ", apellido=" + apellido + ", dni=" + dni + ", fNac=" + fNac + ", estado=" + estado + '}';
-    }
+    
 }
