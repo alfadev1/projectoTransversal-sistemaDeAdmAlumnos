cc65388470cf4861b133087172e7620aa8f00a44
alfadev1 <alfarollano@gmail.com>
2023-09-08 12:49:30 -0300

    clase alumnoData

--- App/appGestionUniversidad/src/AccesoADatos/AlumnoData.java
+++ App/appGestionUniversidad/src/AccesoADatos/AlumnoData.java
@@ -8,6 +8,40 @@ package AccesoADatos;
  *
  * @author cyka
  */
+import Entidades.Alumno;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Date;
+import java.sql.*;
+
+import javax.swing.JOptionPane;
+import org.mariadb.jdbc.Statement;
 public class AlumnoData {
+    
+    private Connection con = null;
+    
+    public AlumnoData() {
+        con = Conexion.getConexion();
+    }
+    
+    public void guardarAlumno(Alumno alumno) {
+        
+        String sql = "INSERT INTO `alumno`(`dni`, `apellido`, `nombre`, `fechaNac`, `estado`) "
+                + "VALUES (?, ?, ?, ?, ?)";
+        try {
+            PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setInt(1, alumno.getDni());
+            ps.setString(2, alumno.getApellido());
+            ps.setString(3, alumno.getNombre());
+            ps.setDate(4, java.sql.Date.valueOf(alumno.getfNac()));
+            ps.setBoolean(5, alumno.isEstado());
+          
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al acceder a la tabla Alumno "+ex.getMessage());
+        }
+        
+    }
+    
     
 }
