c80798d4c380b6b25d9ae935d60c9293156172a9
alfadev1 <alfarollano@gmail.com>
2023-09-12 10:17:35 -0300

    Update Conexion.java

--- App/appGestionUniversidad/src/AccesoADatos/Conexion.java
+++ App/appGestionUniversidad/src/AccesoADatos/Conexion.java
@@ -7,8 +7,7 @@ package AccesoADatos;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+
 import javax.swing.JOptionPane;
 
 /**
